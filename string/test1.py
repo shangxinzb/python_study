973ae72e77c777ccfdf4de3b43730143d77d7f21
wangliang <1198684173@qq.com>
2021-02-23 21:18:44 +0800

    first file

---
+++ string/test1.py
@@ -0,0 +1,22 @@
+word = "字符串"
+sentence = "这是一个句子。"
+paragraph = """
+这是一个段落，
+可以由多行组成
+"""
+
+# 这是注释
+
+num = 1
+print(num)
+
+if num == 1:
+    print(123)
+
+count = 10
+miles = 100.3
+name = 'hello world!'
+
+print(count)
+print(miles)
+print(name)
